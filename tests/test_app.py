c79105e03e2c345516fcbc1c9defefa3103cc1d7
rudolf <rudolf.degeijter@meemoo.be>
2020-05-19 16:57:54 +0200

    Add mocks for rabbit channel and method.

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,15 +12,18 @@ EVENTS_TO_TEST = ["essenceLinkedEvent", "getMetadataResponse"]
 @pytest.mark.parametrize("event", EVENTS_TO_TEST)
 def test_handle_message(event, mock_rabbit, mocker):
     # ARRANGE
-    xml = resources.load_xml_resource(event)
-
     eventListener = EventListener()
-    spy = mocker.spy(eventListener.rabbitClient, "send_message")
-
     routing_key = f"event.to.viaa.{event}"
 
+    xml = resources.load_xml_resource(event)
+    spy = mocker.spy(eventListener.rabbitClient, "send_message")
+    channel_mock = mocker.MagicMock()
+    channel_mock.basic_ack = lambda arg: None
+    method_mock = mocker.MagicMock()
+    method_mock.delivery_tag = 1
+
     # ACT
-    eventListener.handle_message(None, None, None, xml)
+    eventListener.handle_message(channel_mock, method_mock, None, xml)
 
     # ASSERT
     spy.assert_called_with(body=xml, routing_key=routing_key)
