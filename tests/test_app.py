d91d941db56c3f18e47264e1b033a3284d827414
rudolf <rudolf.degeijter@meemoo.be>
2020-05-20 16:30:06 +0200

    [TEST] Fix routing key prefix to be dynamic.

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,6 +2,7 @@ import pytest
 
 from tests.resources import resources
 from tests.resources.mocks import mock_rabbit
+from viaa.configuration import ConfigParser
 
 from app.app import EventListener
 
@@ -12,8 +13,9 @@ EVENTS_TO_TEST = ["essenceLinkedEvent", "getMetadataResponse"]
 @pytest.mark.parametrize("event", EVENTS_TO_TEST)
 def test_handle_message(event, mock_rabbit, mocker):
     # ARRANGE
+    prefix = ConfigParser.app_cfg["rabbitmq"]["routing_key"]
     eventListener = EventListener()
-    routing_key = f"event.to.viaa.{event}"
+    routing_key = f"{prefix}.{event}"
 
     xml = resources.load_xml_resource(event)
     spy = mocker.spy(eventListener.rabbitClient, "send_message")
